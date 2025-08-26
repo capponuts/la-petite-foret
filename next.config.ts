73b4fc17fea68935c73930d93d8285a194e5949d
capponuts <cappk3v@gmail.com>
2025-08-26 13:35:12 +0200

    feat(seo): redirection 308 apex -> www via next.config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,22 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'la-petite-foret.fr',
+          },
+        ],
+        destination: 'https://www.la-petite-foret.fr/:path*',
+        permanent: true,
+        statusCode: 308,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
